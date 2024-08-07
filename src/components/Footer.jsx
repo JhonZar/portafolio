c8de17d3b67a0f7bfd0c0f1303ee9b677ead6b95
Jhonatan Zarzuri Chauca <jzarzuri@procuraduria.gob.bo>
2024-08-07 16:57:32 -0400

    agrega number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-gray-100 py-4">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 Jhonatan Zarzuri.</p>
+        <p>&copy; 2024 Jhonatan Zarzuri +591 72091695 .</p>
       </div>
     </footer>
   );
