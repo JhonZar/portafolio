e462745d294176a72ea392f22a39a03d38d2b772
Jhonatan Zarzuri Chauca <jzarzuri@procuraduria.gob.bo>
2024-08-07 10:40:22 -0400

    ajuste del icono y titulo de la pagina

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   <div className="relative z-10 w-full max-w-lg mx-auto text-center glassmorphism p-8">
    <h2 className="text-4xl font-bold mb-4">Sobre Mí</h2>
         <p className="text-lg mb-4">
-          Hola, soy [Tu Nombre], un desarrollador web apasionado por crear experiencias digitales únicas y efectivas.
+          Hola, soy Jhonatan Zarzuri, un desarrollador web apasionado por crear experiencias digitales únicas y efectivas.
           Tengo experiencia en tecnologías como HTML, CSS, JavaScript, React, y Laravel. Me encanta aprender nuevas
           herramientas y mejorar continuamente mis habilidades.
         </p>
